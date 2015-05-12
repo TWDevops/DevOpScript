cab72d18864b1e15c227d9fd23e7cc93e2953703
evanwu801 <evanwu801@gmail.com>
2015-05-12 15:22:15 +0800

    2015/05/12 15:21 anb_deploy.sh output message format.

--- anb_deploy.sh
+++ anb_deploy.sh
@@ -23,77 +23,86 @@ home=/home/deploy
 pushlog=`ansible $1 -s -m copy -a "src=$home/getlog.sh dest=$home/getlog.sh owner=root group=root mode=755"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $pushlog == "success" ];then
 	sleep 2
-	echo push getlog sehll OK!
+	echo '{action:push,name:getlog,status:OK}'
 else
-	echo push getlog sehll FAILED!
+	echo '{action:push,name:getlog,status:FAILED}'
 fi
 
 #複製檢查shell 過去
 pushcheck=`ansible $1 -s -m copy -a "src=$home/check.sh dest=$home/check.sh owner=root group=root mode=755"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $pushcheck == "success" ];then
-        echo pull check shell OK!
+        echo '{action:push,name:checklog,status:OK}'
 else
-        echo pull check shell FAILED!
+        echo '{action:push,name:checklog,status:FAILED}'
 fi
 
 #複製codeback shell 過去
 pushcodeback=`ansible $1 -s -m copy -a "src=$home/codeback.sh dest=$home/codeback.sh owner=root group=root mode=755"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $pushcodeback == "success" ];then
-        echo push codeback shell OK!
+        echo '{action:push,name:codebackup,status:OK}'
 else
-        echo push codeback shell FAILED!
+        echo '{action:push,name:codebackup,status:FAILED}'
 fi
 
 #複製還原code shell 過去
 pushreback=`ansible $1 -s -m copy -a "src=$home/recode.sh dest=$home/recode.sh owner=root group=root mode=755"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $pushreback == "success" ];then
-        echo push codereback shell OK!
+        echo '{action:push,name:codereback,status:OK}'
 else
-        echo push codereback shell FAILED!
+        echo '{action:push,name:codereback,status:FAILED}'
 fi
+
+#backup code
+backupcode=`ansible $1 -s -m shell -a "$home/codeback.sh"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
+if [ $backupcode == "success" ];then
+        echo '{action:run,name:codebackup,status:OK}'
+else
+        echo '{action:run,name:codebackup,status:FAILED}'
+fi
+
 
 #執行讀取log shell
 runget=`ansible $1 -s -m shell -a "$home/getlog.sh"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $runget == "success" ];then
 	sleep 2
-	echo run getlog OK!
+	echo '{action:run,name:getlog,status:OK}'
 else
-        echo run getlog FAILED!
+        echo '{action:run,name:getlog,status:FAILED}'
 fi
 
 #從git 下載程式
 gitcode=`ansible $1 -s -m git -a "repo=$2 dest=/tmp/$tmpdir"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $gitcode == "success" ];then
 	aar=`ansible $1 -s -a "ls /tmp/$tmpdir"|grep aar`
-	echo git clone code OK!
+	echo '{action:run,name:gitcode,status:OK}'
 else
-        echo git clone code FAILED!
+        echo '{action:run,name:gitcode,status:FAILED}'
 fi
 
 #將程式搬移至執行目錄
 upcode=`ansible $1 -s -a "cp /tmp/$tmpdir/$aar $codedir"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $upcode == "success" ];then
-	echo upload code OK!
+	echo '{action:run,name:uploadcode,status:OK}'
 else
-        echo upload code FAILED!
+        echo '{action:run,name:uploadcode,status:FAILED}'
 fi
 
 #刪除暫存下載目錄
 rmtmp=`ansible $1 -s -a "rm -rf /tmp/$tmpdir"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $rmtmp == "success" ];then
-	echo clear tmp OK!
+	echo '{action:run,name:cleartmp,status:OK}'
 else
-        echo clear tmp FAILED!
+        echo '{action:run,name:cleartmp,status:FAILED}'
 fi
 
 #執行檢查shell
 runcheck=`ansible $1 -s -m shell -a "$home/check.sh $aar"|grep success|cut -d '|' -f2|cut -d ' ' -f2|cut -d ' ' -f1`
 if [ $runcheck == "success" ];then
-	echo run check shell ok!
+	echo '{action:run,name:checklog,status:OK}'
 else
-        echo run check shell FAILED!
+        echo '{action:run,name:checklog,status:FAILED}'
 	ansible $1 -s -m shell -a "$home/recode.sh $codedir"
-	echo restore code to last version!!
+	echo '{action:run,name:restorecode,status:OK}'
 fi
 	
 #掛上nginx
